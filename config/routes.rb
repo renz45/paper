dafe7dac73a47ce9e6697dea427fa5150529dec9
Adam Rensel <adamrensel@gmail.com>
2012-10-07 21:30:24 -0400

    Add admin controllers and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Paper::Application.routes.draw do
   devise_for :users
 
-  root to: "posts#index"
+  root to: 'posts#index'
 
   resources :posts, only: [:show]
+
+  namespace :admin do
+    root to: 'posts#index'
+    resources :posts
+  end
 end
